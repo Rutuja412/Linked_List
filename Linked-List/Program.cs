647a923835b5e2acafee2447b18bc30711a4eaaf
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-19 12:06:05 +0530

    [Rutuja]:Create simple linked list

---
+++ Linked-List/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********Linked List Practice Problems *******");
+            LinkedLists list = new LinkedLists();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
